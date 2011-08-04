b480703c710592d994bf9a349bd51cea4bfda903
Jeff Schmitz <jschmitz65@gmail.com>
2011-08-04 01:49:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colabweb::Application.config.secret_token = '275c0fbd4e57bdbfe19ceb267f2f587fdddd8d68ebfc93fad7b0ef159417a6d8a48af265360bcfe800254f8edf2e0b31e3ca580f3bdd497fd20d8ea295437cf7'
